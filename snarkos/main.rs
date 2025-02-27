0c49d7085ce247b7418387cc1c23c38cab4263f1
edwinmanuelreilly <edwinmanuelreilly@gmail.com>
2025-02-26 21:33:07 -0500

    Verification

--- snarkos/main.rs
+++ snarkos/main.rs
@@ -63,4 +63,5 @@ fn check_for_version() {
         }
     }
 }
-snarkos account sign --raw --private-key-file <fileWithYourAleoKey> -m <verificationHash>
+snarkos account sign --raw --private-key-file <fileWithYourAleoKey> -m <b49c8eab9f24502b05a8f0ae65aab1170e813c3e80edb59e7aeef50caa1f247f>
+b49c8eab9f24502b05a8f0ae65aab1170e813c3e80edb59e7aeef50caa1f247f
