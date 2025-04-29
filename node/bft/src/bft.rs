5d6cc6dcce1e7b5b34257c65a7a9a032b6ac533d
Kai Mast <kai@provable.com>
2025-04-29 20:18:57 +0200

    Improve sync documentation and logging

--- node/bft/src/bft.rs
+++ node/bft/src/bft.rs
@@ -466,14 +466,17 @@ impl<N: Network> BFT<N> {
         // Acquire the BFT lock.
         let _lock = self.lock.lock().await;
 
-        // Retrieve the certificate round.
+        // Retrieve the round of the new certificate to add to the DAG.
         let certificate_round = certificate.round();
+
         // Insert the certificate into the DAG.
         self.dag.write().insert(certificate);
 
-        // Construct the commit round.
+        // Get the previous round number.
         let commit_round = certificate_round.saturating_sub(1);
-        // If the commit round is odd, return early.
+
+        // Leaders are elected in even rounds.
+        // If the previous round is odd, the current round cannot commit any leader certs.
         if commit_round % 2 != 0 || commit_round < 2 {
             return Ok(());
         }
