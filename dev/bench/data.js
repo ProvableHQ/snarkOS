4ff79cf4289b6eb3a56b747d3945aad12a1e0f0d
github-action-benchmark <github@users.noreply.github.com>
2025-08-14 21:40:42 +0000

    add snarkOS Benchmarks (customSmallerIsBetter) benchmark result for c87c4ab38f437da02d076abb1074691a9f5aadde

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1755207642888,
+  "repoUrl": "https://github.com/ProvableHQ/snarkOS",
+  "entries": {
+    "snarkOS Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "kai@provable.com",
+            "name": "Kai Mast",
+            "username": "kaimast"
+          },
+          "committer": {
+            "email": "kai@provable.com",
+            "name": "Kai Mast",
+            "username": "kaimast"
+          },
+          "distinct": true,
+          "id": "c87c4ab38f437da02d076abb1074691a9f5aadde",
+          "message": "ci: add sync benchmark",
+          "timestamp": "2025-08-14T14:10:40-07:00",
+          "tree_id": "c530b1f7d02663e6f3b95d4c33ca755206893e30",
+          "url": "https://github.com/ProvableHQ/snarkOS/commit/c87c4ab38f437da02d076abb1074691a9f5aadde"
+        },
+        "date": 1755207642550,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "p2p-sync",
+            "value": 4.34,
+            "unit": "blocks/s",
+            "extra": "total_wait=46s"
+          }
+        ]
+      }
+    ]
+  }
+}
