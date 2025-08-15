add27732719c42b83a40433335d50d8ddd439aa3
github-action-benchmark <github@users.noreply.github.com>
2025-08-15 03:24:51 +0000

    add snarkOS Benchmarks (customSmallerIsBetter) benchmark result for 8dee5ad57797adffb9efed571bf04b41da69aa90

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1755207642888,
+  "lastUpdate": 1755228291848,
   "repoUrl": "https://github.com/ProvableHQ/snarkOS",
   "entries": {
     "snarkOS Benchmarks": [
@@ -30,6 +30,42 @@ window.BENCHMARK_DATA = {
             "value": 4.34,
             "unit": "blocks/s",
             "extra": "total_wait=46s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kai@provable.com",
+            "name": "Kai Mast",
+            "username": "kaimast"
+          },
+          "committer": {
+            "email": "kai@provable.com",
+            "name": "Kai Mast",
+            "username": "kaimast"
+          },
+          "distinct": true,
+          "id": "8dee5ad57797adffb9efed571bf04b41da69aa90",
+          "message": "ci: add sync benchmark",
+          "timestamp": "2025-08-14T19:46:26-07:00",
+          "tree_id": "a183239d41a40a7a99b85cf0ff48b3a9a0bfaa39",
+          "url": "https://github.com/ProvableHQ/snarkOS/commit/8dee5ad57797adffb9efed571bf04b41da69aa90"
+        },
+        "date": 1755228291523,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "p2p-sync",
+            "value": 4.44,
+            "unit": "blocks/s",
+            "extra": "total_wait=45s"
+          },
+          {
+            "name": "cdn-sync",
+            "value": 0.54,
+            "unit": "blocks/s",
+            "extra": "total_wait=183s"
           }
         ]
       }
