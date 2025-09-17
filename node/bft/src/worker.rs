03023168268dcf7945905eb5d3393bd08baaca3e
Kai Mast <kai@provable.com>
2025-09-17 21:20:42 +0000

    Revert "Merge pull request #3847 from ProvableHQ/fix/revert-pending-blocks"
    This reverts commit de856fd42d6caa022810742329d7fd7c51e8913c, reversing
    changes made to 647f6f325420d994104a9bd3c1062984653e7d1f.

--- node/bft/src/worker.rs
+++ node/bft/src/worker.rs
@@ -26,7 +26,7 @@ use snarkos_node_bft_ledger_service::LedgerService;
 use snarkvm::{
     console::{network::Network, prelude::Read},
     ledger::{
-        block::Transaction,
+        Transaction,
         narwhal::{BatchHeader, Data, Transmission, TransmissionID},
         puzzle::{Solution, SolutionID},
     },
@@ -577,7 +577,8 @@ mod tests {
             types::{Address, Field},
         },
         ledger::{
-            block::Block,
+            Block,
+            PendingBlock,
             committee::Committee,
             narwhal::{BatchCertificate, Subdag, Transmission, TransmissionID},
             test_helpers::sample_execution_transaction_with_fee,
@@ -646,6 +647,8 @@ mod tests {
                 transaction_id: N::TransactionID,
                 transaction: Transaction<N>,
             ) -> Result<()>;
+            fn check_block_subdag(&self, _block: Block<N>, _prefix: &[PendingBlock<N>]) -> Result<PendingBlock<N>>;
+            fn check_block_content(&self, _block: PendingBlock<N>) -> Result<Block<N>>;
             fn check_next_block(&self, block: &Block<N>) -> Result<()>;
             fn prepare_advance_to_next_quorum_block(
                 &self,
