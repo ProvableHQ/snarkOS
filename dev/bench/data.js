0dcb9190ff8ebbbfd2a85928b2cb8d474fe9d404
github-action-benchmark <github@users.noreply.github.com>
2025-10-17 14:03:15 +0000

    add snarkOS Benchmarks (customBiggerIsBetter) benchmark result for 8196a9821ab6ca72f4a33c9fcc56bd2c61e663b5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1760703405595,
+  "lastUpdate": 1760709795221,
   "repoUrl": "https://github.com/ProvableHQ/snarkOS",
   "entries": {
     "snarkOS Benchmarks": [
@@ -3822,6 +3822,66 @@ window.BENCHMARK_DATA = {
             "value": 1.22,
             "unit": "blocks/s",
             "extra": "total_wait=204s, target_height=250"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "victor.s.nicolaas@protonmail.com",
+            "name": "vicsn",
+            "username": "vicsn"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8196a9821ab6ca72f4a33c9fcc56bd2c61e663b5",
+          "message": "Merge pull request #3950 from ljedrz/logs/dont_error_on_dupe_addr_as_responder\n\n[Logs] Don't return an error when the Aleo address is already connecte…",
+          "timestamp": "2025-10-17T15:32:41+02:00",
+          "tree_id": "4955009437597e952e228a82b12ac9727072bd35",
+          "url": "https://github.com/ProvableHQ/snarkOS/commit/8196a9821ab6ca72f4a33c9fcc56bd2c61e663b5"
+        },
+        "date": 1760709794063,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "rest-get-block",
+            "value": 2.636881906621977,
+            "unit": "ops/s",
+            "extra": "num_ops=480, total_wait=182.03318047523499, endpoint=http://localhost:3030/v2/testnet/block, num_validators=40, git_commit=9ec2291c57, snapshot_height=250"
+          },
+          {
+            "name": "rest-block-height",
+            "value": 7552.771310750896,
+            "unit": "ops/s",
+            "extra": "num_ops=80000, total_wait=10.592138528823853, endpoint=http://localhost:3030/v2/testnet/block/height/latest, num_validators=40, git_commit=9ec2291c57, snapshot_height=250"
+          },
+          {
+            "name": "p2p-sync",
+            "value": 1.35,
+            "unit": "blocks/s",
+            "extra": "total_wait=184s, target_height=250, connect_time=22s, num_validators=40, git_commit=9ec2291c57, snapshot_height=250"
+          },
+          {
+            "name": "p2p-sync-speed-variance",
+            "value": 1.267599,
+            "unit": "blocks^2/s^2",
+            "extra": "samples=179, mean_speed=0.865084, max_speed=3.066667, branch=staging, num_validators=40, git_commit=9ec2291c57, snapshot_height=250"
+          },
+          {
+            "name": "bft-sync",
+            "value": 1.21,
+            "unit": "blocks/s",
+            "extra": "total_wait=205s, target_height=250, connect_time=0, branch=staging, num_validators=40, git_commit=9ec2291c57, snapshot_height=250"
+          },
+          {
+            "name": "cdn-sync",
+            "value": 1.23,
+            "unit": "blocks/s",
+            "extra": "total_wait=203s, target_height=250"
           }
         ]
       }
