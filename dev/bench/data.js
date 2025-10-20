af9e659742510a51e87a20973649a5d10c8ba31e
github-action-benchmark <github@users.noreply.github.com>
2025-10-20 12:00:21 +0000

    add snarkOS Benchmarks (customBiggerIsBetter) benchmark result for b6ede3bdceab70b30df3f74fd5efc3c5905453bc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1760777099550,
+  "lastUpdate": 1760961621058,
   "repoUrl": "https://github.com/ProvableHQ/snarkOS",
   "entries": {
     "snarkOS Benchmarks": [
@@ -3942,6 +3942,66 @@ window.BENCHMARK_DATA = {
             "value": 1.23,
             "unit": "blocks/s",
             "extra": "total_wait=203s, target_height=250"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "victor.s.nicolaas@protonmail.com",
+            "name": "vicsn",
+            "username": "vicsn"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b6ede3bdceab70b30df3f74fd5efc3c5905453bc",
+          "message": "Merge pull request #3942 from ljedrz/feat/tighten_bootstrap_validator_sharing\n\n[Feat] Tighten BootstrapClient validator sharing",
+          "timestamp": "2025-10-20T13:25:33+02:00",
+          "tree_id": "ea06c4c1a912fcac27cc1ee48c9178da7f77a4b2",
+          "url": "https://github.com/ProvableHQ/snarkOS/commit/b6ede3bdceab70b30df3f74fd5efc3c5905453bc"
+        },
+        "date": 1760961620383,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "rest-get-block",
+            "value": 2.61391865328756,
+            "unit": "ops/s",
+            "extra": "num_ops=480, total_wait=183.63234043121338, endpoint=http://localhost:3030/v2/testnet/block, num_validators=40, git_commit=9ec2291c57, snapshot_height=250"
+          },
+          {
+            "name": "rest-block-height",
+            "value": 7904.296163101829,
+            "unit": "ops/s",
+            "extra": "num_ops=80000, total_wait=10.121078252792358, endpoint=http://localhost:3030/v2/testnet/block/height/latest, num_validators=40, git_commit=9ec2291c57, snapshot_height=250"
+          },
+          {
+            "name": "p2p-sync",
+            "value": 1.34,
+            "unit": "blocks/s",
+            "extra": "total_wait=186s, target_height=250, connect_time=21s, num_validators=40, git_commit=9ec2291c57, snapshot_height=250"
+          },
+          {
+            "name": "p2p-sync-speed-variance",
+            "value": 1.311606,
+            "unit": "blocks^2/s^2",
+            "extra": "samples=181, mean_speed=0.876980, max_speed=2.983333, branch=staging, num_validators=40, git_commit=9ec2291c57, snapshot_height=250"
+          },
+          {
+            "name": "bft-sync",
+            "value": 1.23,
+            "unit": "blocks/s",
+            "extra": "total_wait=203s, target_height=250, connect_time=0, branch=staging, num_validators=40, git_commit=9ec2291c57, snapshot_height=250"
+          },
+          {
+            "name": "cdn-sync",
+            "value": 1.21,
+            "unit": "blocks/s",
+            "extra": "total_wait=205s, target_height=250"
           }
         ]
       }
