6b37b7998b59ba0ff711b53e1b02697c0c59ad8f
github-action-benchmark <github@users.noreply.github.com>
2025-10-23 13:05:10 +0000

    add snarkOS Benchmarks (customBiggerIsBetter) benchmark result for b5655780eba992c05c3daf501e474a1ecf8abeba

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1761213747149,
+  "lastUpdate": 1761224710145,
   "repoUrl": "https://github.com/ProvableHQ/snarkOS",
   "entries": {
     "snarkOS Benchmarks": [
@@ -4416,6 +4416,66 @@ window.BENCHMARK_DATA = {
             "value": 1.14,
             "unit": "blocks/s",
             "extra": "total_wait=218s, target_height=250, connect_time=0, branch=staging, num_validators=40, git_commit=9ec2291c57, snapshot_height=250"
+          },
+          {
+            "name": "cdn-sync",
+            "value": 1.23,
+            "unit": "blocks/s",
+            "extra": "total_wait=203s, target_height=250"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "victor.s.nicolaas@protonmail.com",
+            "name": "vicsn",
+            "username": "vicsn"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b5655780eba992c05c3daf501e474a1ecf8abeba",
+          "message": "Merge pull request #3963 from ljedrz/fix/locktick_in_node_network\n\n[Fix] Inherit the locktick feature in snarkos-node-network",
+          "timestamp": "2025-10-23T14:34:52+02:00",
+          "tree_id": "4f1fb6369da4c2f4e0c429782fcfd14947a7e34b",
+          "url": "https://github.com/ProvableHQ/snarkOS/commit/b5655780eba992c05c3daf501e474a1ecf8abeba"
+        },
+        "date": 1761224709294,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "rest-get-block",
+            "value": 2.6159179470840046,
+            "unit": "ops/s",
+            "extra": "num_ops=480, total_wait=183.49199390411377, endpoint=http://localhost:3030/v2/testnet/block, num_validators=40, git_commit=9ec2291c57, snapshot_height=250"
+          },
+          {
+            "name": "rest-block-height",
+            "value": 8002.754396497437,
+            "unit": "ops/s",
+            "extra": "num_ops=80000, total_wait=9.99655818939209, endpoint=http://localhost:3030/v2/testnet/block/height/latest, num_validators=40, git_commit=9ec2291c57, snapshot_height=250"
+          },
+          {
+            "name": "p2p-sync",
+            "value": 1.35,
+            "unit": "blocks/s",
+            "extra": "total_wait=184s, target_height=250, connect_time=21s, num_validators=40, git_commit=9ec2291c57, snapshot_height=250"
+          },
+          {
+            "name": "p2p-sync-speed-variance",
+            "value": 1.292009,
+            "unit": "blocks^2/s^2",
+            "extra": "samples=179, mean_speed=0.827374, max_speed=3.333333, branch=staging, num_validators=40, git_commit=9ec2291c57, snapshot_height=250"
+          },
+          {
+            "name": "bft-sync",
+            "value": 1.2,
+            "unit": "blocks/s",
+            "extra": "total_wait=208s, target_height=250, connect_time=0, branch=staging, num_validators=40, git_commit=9ec2291c57, snapshot_height=250"
           },
           {
             "name": "cdn-sync",
