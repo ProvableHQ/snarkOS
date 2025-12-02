4cd95b4fb2f4f934a083915cbbdafef8631926f2
Kai Mast <kai@kaimast.com>
2025-12-01 17:26:20 -0800

    test(node/bft): fix test failing due to blocking tasks

--- node/bft/tests/components/worker.rs
+++ node/bft/tests/components/worker.rs
@@ -36,7 +36,10 @@ async fn test_resend_transmission_request() {
     // Initialize the accounts and the committee.
     let (accounts, committee) = new_test_committee(num_nodes, &mut rng);
     // Sample a ledger.
-    let ledger = sample_ledger(&accounts, &committee, &mut rng);
+    let ledger = {
+        let accounts = accounts.clone();
+        tokio::task::spawn_blocking(move || sample_ledger(&accounts, &committee, &mut rng)).await.unwrap()
+    };
     // Sample a worker.
     let worker = sample_worker(0, accounts[0].clone(), ledger.clone());
 
@@ -118,7 +121,10 @@ async fn test_flood_transmission_requests() {
     // Initialize the accounts and the committee.
     let (accounts, committee) = new_test_committee(num_nodes, &mut rng);
     // Sample a ledger.
-    let ledger = sample_ledger(&accounts, &committee, &mut rng);
+    let ledger = {
+        let accounts = accounts.clone();
+        tokio::task::spawn_blocking(move || sample_ledger(&accounts, &committee, &mut rng)).await.unwrap()
+    };
     // Sample a worker.
     let worker = sample_worker(0, accounts[0].clone(), ledger.clone());
 
