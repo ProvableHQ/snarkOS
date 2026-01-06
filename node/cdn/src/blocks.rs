a024b52513b23837c04dd1ef54ef1f35fef3e191
Victor Sint Nicolaas <vicsn@users.noreply.github.com>
2026-01-06 10:42:08 +0100

    Pretty print error when CDN is unavailable

--- node/cdn/src/blocks.rs
+++ node/cdn/src/blocks.rs
@@ -448,7 +448,10 @@ async fn cdn_height<const BLOCKS_PER_FILE: u32>(client: &Client, base_url: &http
     // Parse the bytes for the string.
     let latest_state_string = match bincode::deserialize::<String>(&bytes) {
         Ok(string) => string,
-        Err(error) => bail!("Failed to deserialize the CDN height response - {error}"),
+        Err(error) => {
+            let bytes_as_string = String::from_utf8_lossy(&bytes);
+            bail!("Failed to deserialize the CDN height response - {error} - {bytes_as_string}")
+        }
     };
     // Parse the string for the tip.
     let tip = match serde_json::from_str::<LatestState>(&latest_state_string) {
