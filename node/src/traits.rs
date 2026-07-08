b322ecd9d089c36bf448dc358a37d2c5077b6290
ljedrz <ljedrz@users.noreply.github.com>
2026-07-08 14:52:33 +0200

    fix: account for other platforms in the shutdown task check
    Signed-off-by: ljedrz <ljedrz@users.noreply.github.com>

--- node/src/traits.rs
+++ node/src/traits.rs
@@ -61,10 +61,15 @@ pub trait NodeInterface<N: Network>: Routing<N> {
         // Check if there are any stragglers left.
         if let Some(handle) = &handler.handle {
             let live_tasks = handle.metrics().num_alive_tasks();
-            // The 1 "extra" task comes from the FD monitor, which is aborted once
-            // wait_for_signals concludes its work.
-            if live_tasks != 1 {
-                error!("There are still {} live tasks", live_tasks - 1);
+            let expected_leftover = if cfg!(unix) {
+                // The 1 "extra" task comes from the FD monitor, which is aborted once
+                // wait_for_signals concludes its work.
+                1
+            } else {
+                0
+            };
+            if live_tasks > expected_leftover {
+                error!("There are {} surplus live tasks", live_tasks - expected_leftover);
             }
         }
     }
