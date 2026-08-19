a5a18ac5cca6159ebba4298ed24645ca121ac5de
github-action-benchmark <github@users.noreply.github.com>
2026-08-19 09:58:19 +0000

    add snarkOS Benchmarks (customBiggerIsBetter) benchmark result for b184df670852fa023435aea49f90b96d829892ef

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787133441976,
+  "lastUpdate": 1787133499105,
   "repoUrl": "https://github.com/ProvableHQ/snarkOS",
   "entries": {
     "snarkOS Benchmarks": [
@@ -16272,6 +16272,72 @@ window.BENCHMARK_DATA = {
             "value": 1.63,
             "unit": "blocks/s",
             "extra": "total_wait=153s, target_height=250"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "victor.s.nicolaas@protonmail.com",
+            "name": "vicsn",
+            "username": "vicsn"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b184df670852fa023435aea49f90b96d829892ef",
+          "message": "Merge pull request #4385 from ProvableHQ/feat/introduce_a_per_ip_conn_limit\n\n[Feat] Introduce a per-IP connection limit",
+          "timestamp": "2026-08-19T11:38:31+02:00",
+          "tree_id": "a04f080c1269a8b16a5d114abc57b21c9bb713fa",
+          "url": "https://github.com/ProvableHQ/snarkOS/commit/b184df670852fa023435aea49f90b96d829892ef"
+        },
+        "date": 1787133498298,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "rest-get-block",
+            "value": 9.23420775937657,
+            "unit": "ops/s",
+            "extra": "num_ops=480, total_wait=51.98063683509827, endpoint=http://localhost:3030/v2/testnet/block, num_validators=40, git_commit=9ec2291c57, snapshot_height=250"
+          },
+          {
+            "name": "rest-block-height",
+            "value": 7276.5857885385985,
+            "unit": "ops/s",
+            "extra": "num_ops=80000, total_wait=10.99416708946228, endpoint=http://localhost:3030/v2/testnet/block/height/latest, num_validators=40, git_commit=9ec2291c57, snapshot_height=250"
+          },
+          {
+            "name": "rest-get-latest-block",
+            "value": 2952.838442550458,
+            "unit": "ops/s",
+            "extra": "num_ops=800, total_wait=0.27092576026916504, endpoint=http://localhost:3030/v2/testnet/block, num_validators=40, git_commit=9ec2291c57, snapshot_height=250"
+          },
+          {
+            "name": "p2p-sync",
+            "value": 1.82,
+            "unit": "blocks/s",
+            "extra": "total_wait=137s, target_height=250, connect_time=0s, num_validators=40, git_commit=9ec2291c57, snapshot_height=250"
+          },
+          {
+            "name": "p2p-sync-speed-variance",
+            "value": 1.276632,
+            "unit": "blocks^2/s^2",
+            "extra": "samples=134, mean_speed=1.258333, max_speed=2.850000, branch=staging, num_validators=40, git_commit=9ec2291c57, snapshot_height=250"
+          },
+          {
+            "name": "bft-sync",
+            "value": 1.66,
+            "unit": "blocks/s",
+            "extra": "total_wait=150s, target_height=250, connect_time=13, branch=staging, num_validators=40, git_commit=9ec2291c57, snapshot_height=250"
+          },
+          {
+            "name": "cdn-sync",
+            "value": 1.64,
+            "unit": "blocks/s",
+            "extra": "total_wait=152s, target_height=250"
           }
         ]
       }
