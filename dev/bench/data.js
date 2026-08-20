9092b5cd6a527c132ab787144281d159493bbb7d
github-action-benchmark <github@users.noreply.github.com>
2026-08-20 06:36:54 +0000

    add snarkOS Benchmarks (customBiggerIsBetter) benchmark result for 0c81e4370af3afcfdbded96eb7b0be7dc9f39c23

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787207404402,
+  "lastUpdate": 1787207814359,
   "repoUrl": "https://github.com/ProvableHQ/snarkOS",
   "entries": {
     "snarkOS Benchmarks": [
@@ -16404,6 +16404,72 @@ window.BENCHMARK_DATA = {
             "value": 2,
             "unit": "blocks/s",
             "extra": "total_wait=125s, target_height=250"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "victor.s.nicolaas@protonmail.com",
+            "name": "vicsn",
+            "username": "vicsn"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0c81e4370af3afcfdbded96eb7b0be7dc9f39c23",
+          "message": "Merge pull request #4401 from cbeck88/test/bft-memory-storage-service\n\ntest(bft): cover the in-memory storage service, and fix a deadlock in it",
+          "timestamp": "2026-08-20T08:17:40+02:00",
+          "tree_id": "f4254f51fe08e20ea6ab7ea33c95ac8e864aa071",
+          "url": "https://github.com/ProvableHQ/snarkOS/commit/0c81e4370af3afcfdbded96eb7b0be7dc9f39c23"
+        },
+        "date": 1787207813105,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "rest-get-block",
+            "value": 9.866759345430664,
+            "unit": "ops/s",
+            "extra": "num_ops=480, total_wait=48.648191690444946, endpoint=http://localhost:3030/v2/testnet/block, num_validators=40, git_commit=9ec2291c57, snapshot_height=250"
+          },
+          {
+            "name": "rest-block-height",
+            "value": 7048.349386428226,
+            "unit": "ops/s",
+            "extra": "num_ops=80000, total_wait=11.350175142288208, endpoint=http://localhost:3030/v2/testnet/block/height/latest, num_validators=40, git_commit=9ec2291c57, snapshot_height=250"
+          },
+          {
+            "name": "rest-get-latest-block",
+            "value": 2065.0545982476124,
+            "unit": "ops/s",
+            "extra": "num_ops=800, total_wait=0.38739895820617676, endpoint=http://localhost:3030/v2/testnet/block, num_validators=40, git_commit=9ec2291c57, snapshot_height=250"
+          },
+          {
+            "name": "p2p-sync",
+            "value": 1.78,
+            "unit": "blocks/s",
+            "extra": "total_wait=140s, target_height=250, connect_time=0s, num_validators=40, git_commit=9ec2291c57, snapshot_height=250"
+          },
+          {
+            "name": "p2p-sync-speed-variance",
+            "value": 1.198855,
+            "unit": "blocks^2/s^2",
+            "extra": "samples=136, mean_speed=1.215931, max_speed=2.833333, branch=staging, num_validators=40, git_commit=9ec2291c57, snapshot_height=250"
+          },
+          {
+            "name": "bft-sync",
+            "value": 1.71,
+            "unit": "blocks/s",
+            "extra": "total_wait=146s, target_height=250, connect_time=9, branch=staging, num_validators=40, git_commit=9ec2291c57, snapshot_height=250"
+          },
+          {
+            "name": "cdn-sync",
+            "value": 1.65,
+            "unit": "blocks/s",
+            "extra": "total_wait=151s, target_height=250"
           }
         ]
       }
